b571678727ef9389d465ba0b7dcd545ae290e952
Dhivyadarsshni <85487484+Dhivyadarsshni@users.noreply.github.com>
2021-06-07 16:48:34 +0530

    Created area.py
    this file generates area of the circle

---
+++ area.py
@@ -0,0 +1,3 @@
+num=float(input("Input the radius of the circle: "))          #Give the radius in the format of int/float
+area=3.14*(num**2)                                            #Generating the circle's area formula
+print("The area of the circle with radius ",num,"is: ",area)  
